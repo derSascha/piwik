b03ba3a9ae2ab1b54db03edfa5b668eb965154d4
Thomas Steur <thomas.steur@gmail.com>
2015-03-02 19:39:14 +0000

    make sure filterSubtables can be executed on DataTable\Map

--- core/DataTable/Map.php
+++ core/DataTable/Map.php
@@ -110,6 +110,19 @@ class Map implements DataTableInterface
     }
 
     /**
+     * Apply a filter to all subtables contained by this instance.
+     *
+     * @param string|Closure $className Name of filter class or a Closure.
+     * @param array $parameters Parameters to pass to the filter.
+     */
+    public function filterSubtables($className, $parameters = array())
+    {
+        foreach ($this->getDataTables() as $table) {
+            $table->filterSubtables($className, $parameters);
+        }
+    }
+
+    /**
      * Returns the array of DataTables contained by this class.
      *
      * @return DataTable[]|Map[]
